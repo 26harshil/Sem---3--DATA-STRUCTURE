d83abd6cef1f6afa8c1d8b3f0d066b6674523aa2
Harshil solanki <146664906+26harshil@users.noreply.github.com>
2024-08-04 13:06:24 +0530

    Create lab_69.java

---
+++ Lab 12/lab_69.java
@@ -0,0 +1,55 @@
+
+public void sort(Linked l1) {
+            Linked s = l1;
+            Stack<Integer> s1 = new Stack<>();
+            if (first == null) {
+                System.out.println("the linkedlist is empty");
+                return;
+            } else if (first.link == null) {
+                System.out.println(first.data + "there is only one element");
+                return;
+            }
+            Node temp = first;
+            int a[] = new int[size];
+            int i = 0;
+
+            while (temp != null) {
+                a[i] = temp.data;
+                i++;
+                temp = temp.link;
+
+            }
+
+            Arrays.sort(a);
+
+            int j = 0;
+            Node flag = first;
+            while (j < size && flag != null) {
+                s1.push(a[j]);
+                flag.data = s1.pop();
+                flag = flag.link;
+                j++;
+
+            }
+
+
+        }
+         ====================================================OR===============================================================
+    public void removeDuplicates() {
+        if (head == null) {
+            return;
+        }
+
+        Node current = head;
+
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+               
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
+   
